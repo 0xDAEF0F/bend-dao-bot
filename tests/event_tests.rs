ad6514f734238b65037a57639069653dac4adadf
daemon <0xDAEF0F@proton.me>
2024-04-13 12:58:21 -0600

    added subscription, too

--- tests/event_tests.rs
+++ tests/event_tests.rs
@@ -12,7 +12,7 @@ use ethers::{
 use std::sync::Arc;
 
 #[tokio::test]
-async fn test_query_past_auction_events() -> Result<()> {
+async fn test_subscribe_parse_past_auction_event() -> Result<()> {
     dotenv()?;
 
     let wss_url = std::env::var("MAINNET_RPC_URL_WS")?;
